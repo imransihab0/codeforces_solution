18aec9a2f040161af037f6fa072240a7bb85d2c0
Imran Sihab <imran.sihab.0@gmail.com>
2024-02-08 05:23:44 +0600

    codeforces solution
    at [ Oct/25/2023 15:50UTC+6 ]

---
+++ 4A - 8.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/contest/4/problem/A
+// @author: imransihab0
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int n;
+    cin >> n;
+
+    if(n>2 && n%2==0){
+        cout << "YES\n";
+    }else{
+        cout << "NO\n";
+    }
+    
+return 0;
+}
