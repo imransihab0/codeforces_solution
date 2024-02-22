b1403e62d6fe46ad28b80425bdf88d626a024876
Imran Sihab <imran.sihab.0@gmail.com>
2024-02-23 02:34:33 +0600

    Create A_Vlad_and_the_Best_of_Five.cpp

---
+++ Codeforces Round 928 (Div. 4)/A_Vlad_and_the_Best_of_Five.cpp
@@ -0,0 +1,31 @@
+// https://codeforces.com/contest/1926/problem/A
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int c = 0, _c = 0;
+        string s;
+        cin >> s;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == 'A'){
+                c++;
+            }else{
+                _c++;
+            }
+        }
+
+        if(c>_c){
+            cout << "A" << endl;
+        }else{
+            cout << "B" << endl;
+        }
+    }
+    
+return 0;
+}
