c6bdd7579cec2ef18a097a83f7fa59eb397df81a
Imran Sihab <imran.sihab.0@gmail.com>
2024-02-28 01:05:54 +0600

    Codeforce Solutions
    at [ Oct/27/2023 15:39UTC+6 ]

---
+++ 236A - 10.cpp
@@ -0,0 +1,35 @@
+// https://codeforces.com/contest/236/problem/A
+// @handle: imransihab0
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    
+    char str[100];
+    scanf("%s", str);
+    
+    for(int i=0; str[i] != '\0'; i++){
+        for(int j=i+1; str[j] != '\0'; j++){
+            if(str[i] > str[j]){
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+
+    int count = 0;
+    for(int i=0; str[i] != '\0'; i++){
+        if(str[i] != str[i+1]){
+            count++;
+        }
+    }
+
+    if(count%2 == 0){
+        printf("CHAT WITH HER!\n");
+    }else{
+        printf("IGNORE HIM!\n");
+    }
+    
+return 0;
+}
