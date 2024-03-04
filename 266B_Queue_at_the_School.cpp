bc3083e3989835d7bc619fb07b36e219e2742c84
Imran Sihab <imran.sihab.0@gmail.com>
2024-03-05 00:10:56 +0600

    Codeforce Solutions
    at [ Mar/05/2024 00:07UTC+6 ]

---
+++ 266B_Queue_at_the_School.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/problemset/problem/266/B
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int n, t;
+    cin >> n >> t;
+    string s;
+    cin >> s;
+
+    for(int j=0; j<t; j++){
+        for(int i=0; i<s.size()-1; i++){
+            if(s[i] == 'B' && s[i+1] == 'G'){
+                swap(s[i], s[i+1]);
+                i++;
+            }
+        }
+    }
+
+    cout << s << endl;
+    
+return 0;
+}
