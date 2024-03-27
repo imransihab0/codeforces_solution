51f82bea8c23b6e8a5eac8a19c821d70a9cba7b6
Imranul Islam Shihab <imran.sihab.0@gmail.com>
2024-03-28 02:59:11 +0600

    Codeforce Solution
    at [ Feb/19/2024 18:45 ]

---
+++ 1926C_Vlad_and_a_Sum_of_Sum_of_Digits.cpp
@@ -0,0 +1,34 @@
+// https://codeforces.com/contest/1926/problem/C
+// @author: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 2*1e5+10;
+long long sum[N];
+
+void func(){
+    for(int i=1; i<=N; i++){
+
+        int num = i;
+        long long summ = 0;
+        while(num>0){
+            summ += num%10;
+            num/=10;
+        }
+        sum[i] = sum[i-1] + summ; 
+    }
+}
+
+int main(){
+
+    func();
+    int t;
+    cin >> t;
+    while (t--){
+
+        int n;
+        cin >> n;
+        cout << sum[n] << endl;
+
+    }  
+}
