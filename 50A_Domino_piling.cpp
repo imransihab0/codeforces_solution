538a929e252cdacc3dc3417684fc825984e6e3b9
Imranul Islam Shihab <imran.sihab.0@gmail.com>
2024-04-04 23:56:37 +0600

    Codeforce Sollution
    at [ Apr/04/2024 22:52UTC+6 ]

---
+++ 50A_Domino_piling.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/50/A
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+#define long long int
+
+int32_t main(){
+
+	int a, b, r;
+	cin >> a >> b;
+	r = (a*b)/2;
+	cout << r << endl;
+
+	return 0;
+}
