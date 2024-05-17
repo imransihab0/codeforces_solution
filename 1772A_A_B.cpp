57cc3bd1bafd049f4bc24afedd37b4da7af397c2
Imranul Islam Shihab <imran.sihab.0@gmail.com>
2024-05-17 17:17:42 +0600

    at [ May/17/2024 15:07UTC+6 ]

---
+++ 1772A_A_B.cpp
@@ -0,0 +1,22 @@
+// https://codeforces.com/problemset/problem/1772/A
+// written in google docs :')
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+int t;
+cin >> t;
+
+while(t--)
+{
+	string s;
+cin >> s;
+cout << (s[0]-'0') + (s[2]-'0') << endl;
+}
+
+return 0;
+}
